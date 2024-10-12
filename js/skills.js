5e4bca973f75684c3da25c0c823fa273c361b787
jabir756 <jabirkagone6@gmail>
2024-10-11 23:40:13 -0400

    responsive

--- js/skills.js
+++ js/skills.js
@@ -16,15 +16,15 @@ for(let i = 1; i < skillsSrcTitles.length-1; i++){
     const div = document.createElement('div');
     const img = document.createElement('img');
     img.className = 'skill-logo';
-    const h4 = document.createElement('h4');
-    h4.className = 'skill-title';
+    const h = document.createElement('h5');
+    h.className = 'skill-title';
     //create skill image
     img.src = `${skillsSrcTitles[i][0]}`;
     img.alt = `${skillsSrcTitles[i][1]}`;
     //create skill title
-    h4.textContent = `${skillsSrcTitles[i][1]}`;
+    h.textContent = `${skillsSrcTitles[i][1]}`;
     //add skill image and title to div
-    div.append(img, h4);
+    div.append(img, h);
     //add div to skills list
     skillsList.append(div);
 }
