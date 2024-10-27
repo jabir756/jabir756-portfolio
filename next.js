832083a71659eabff928fb7ca86b4e3eb9454f8e
jabir756 <jabirkagone6@gmail>
2024-10-27 19:52:06 -0400

    renames files and rewrite js

--- next.js
+++ next.js
@@ -1,58 +1,32 @@
-//Project images sources
-projectImgSrc = [
-    ['project-img/interface.png', 'project 3']
-];
-
-// Project Link
-projectLinks = [
-    'https://github.com/jabir756/home-interface-android.git'
-]
-
-// Project Descriptions 1: Project Name, Description
-projectDetails = [
-    ['Home interface android', 'Create home interface for college la cite using kotlin and xml']
-];
+// Imports
+import { projectsArray } from "./projects.js";
 
 // Get the grid container for projects
-const projectGrid = document.getElementById('project-grid');
-
-// Create elements and append them to the body
-const div = document.createElement('div');
-const img = document.createElement('img');
-const h3 = document.createElement('h3');
-const p = document.createElement('p');
-const a = document.createElement('a');
+const projectsGrid = document.getElementById('projects-grid');
 
 // Set the number of elements to display
-numberElementToDisplay = 0;
+let numberElementToDisplay = 0;
 
 // Function to display the initial cards
 function displayCards(numberElementToDisplay){
-     // Loop through each project and create a new div for each
+
+    // Loop through each project and create display them
     for (let i = 0; i < numberElementToDisplay; i++) {
-        div.classList.add('project-card');
-
-        img.src = projectImgSrc[i][0];
-        img.alt = projectImgSrc[i][1];
-
-        h3.textContent = projectDetails[i][0];
-        p.textContent = projectDetails[i][1];
-
-        a.href = projectLinks[i];
-        a.textContent = 'View Project';
-
-        div.appendChild(img);
-        div.appendChild(h3);
-        div.appendChild(p);
-        div.appendChild(a);
-
-        projectGrid.append(div);
+        projectsGrid.innerHTML += projectsArray[i];
     }
 }
 
 // On next button click call the function to display the initial cards
 document.getElementById('next').addEventListener('click', (e) => {
     e.preventDefault();
-    numberElementToDisplay += 2;
-    displayCards(numberElementToDisplay);
+
+    // If projectsArray length lower than number of elements to display
+    // do nothing
+    if(numberElementToDisplay>=projectsArray.length){
+        return
+    }
+    else{
+        numberElementToDisplay += 2;
+        displayCards(numberElementToDisplay);
+    }
 });
