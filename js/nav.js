528aae688538dc9bf00e41db83a1d17ba884780a
jabir756 <jabirkagone6@gmail>
2024-11-14 19:23:03 -0500

    Refactor navigation and header styles, implement dropdown functionality, and remove unused CSS

---
+++ js/nav.js
@@ -0,0 +1,18 @@
+document.addEventListener('DOMContentLoaded', ()=> {
+
+    let dropdown = false;
+    document.querySelector('#menu-btn').addEventListener('click', ()=> {
+        dropdown = !dropdown;
+        if(dropdown) {
+            document.querySelector('#menu-btn').innerHTML = '&#10005;';
+            document.querySelector('#menu-btn').style.color = '#333333';
+            document.querySelector('.content').style.display = 'flex';
+            document.querySelector('.nav').style.backgroundColor = '#ffffff';
+        } else {
+            document.querySelector('#menu-btn').innerHTML = '&#9776;';
+            document.querySelector('#menu-btn').style.color = '#ffffff';
+            document.querySelector('.content').style.display = 'none';
+            document.querySelector('.nav').style.backgroundColor = 'transparent';
+        }
+    });
+});
