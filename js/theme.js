0a64625b04a4efb1a90ac1d492b08543ae6a8ccd
jabir756 <jabirkagone6@gmail>
2024-11-23 18:08:57 -0500

    Refactor header styles, remove theme toggle functionality, and reorganize certification section

--- js/theme.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', ()=> {
-    document.querySelector('#theme').addEventListener(
-        'click', ()=> {
-            document.querySelector('body').classList.toggle('body-dark');
-            document.querySelector('.content').classList.toggle('content-dark');
-            document.querySelector('.nav').classList.toggle('nav-dark');
-        }
-    )
-});
