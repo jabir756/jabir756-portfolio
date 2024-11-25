929750183f89d9415a2d3f55f62f11151db430ce
jabir756 <jabirkagone6@gmail>
2024-11-25 14:21:05 -0500

    Enhance navigation: add no-scroll class for menu toggle and update font family in styles

--- js/nav.js
+++ js/nav.js
@@ -8,11 +8,13 @@ document.addEventListener('DOMContentLoaded', ()=> {
             document.querySelector('#menu-btn').style.color = '#333333';
             document.querySelector('.content').style.display = 'flex';
             document.querySelector('.introduce').style.display = 'none';
+            document.body.classList.add('no-scroll');
         } else {
             document.querySelector('#menu-btn').innerHTML = '&#9776;';
             document.querySelector('#menu-btn').style.color = '#333333';
             document.querySelector('.content').style.display = 'none';
             document.querySelector('.introduce').style.display = 'flex';
+            document.body.classList.remove('no-scroll');
         }
     });
 });
