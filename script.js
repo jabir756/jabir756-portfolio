0a8d7ae909b5708fcd3a7ff1e4ce4f038a20032d
jabir756 <jabirkagone6@gmail>
2024-12-18 05:22:16 -0500

    Refactor CSS styles and JavaScript functionality: adjust z-index and background colors for navigation; update card hover effects; enhance menu button behavior for improved user experience.

--- script.js
+++ script.js
@@ -8,8 +8,10 @@ document.addEventListener('DOMContentLoaded', () => {
         // Toggle menu button text
         if (menuContent.classList.contains('active')) {
             menuBtn.innerHTML = '&times;';
+            document.body.style.overflow = 'hidden';
         } else {
             menuBtn.innerHTML = '&#8801;';
+            document.body.style.overflow = 'auto';
         }
     });
 
